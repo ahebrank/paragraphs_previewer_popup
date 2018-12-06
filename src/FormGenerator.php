cde74cca1ae14e18014a119ea929d30766242f61
Andy Hebrank <ahebrank@gmail.com>
2018-12-05 21:07:38 -0500

    fix button placement on the experimental widget

--- src/FormGenerator.php
+++ src/FormGenerator.php
@@ -5,62 +5,84 @@ namespace Drupal\paragraphs_previewer_popup;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Field\WidgetBase;
 
+/**
+ * Form creation/modification.
+ */
 class FormGenerator {
-    public static function modifyParagraphsTop(&$element, $form_state, $context) {
-        $element['top']['#attributes']['class'][] = 'paragraphs-previewer--button-added';
-        $element['top']['previewer_container'] = [
-            '#type' => 'container',
-            '#attributes' => [
-                'class' => 'paragraphs-previewer-popup__link-container',
-            ],
-            '#attached' => [
-                'library' => [
-                    'paragraphs_previewer_popup/admin',
-                ],
-            ],
-            'previewer_button' => self::getPreviewButton($context, $element, $form_state),
-        ];
+
+  /**
+   * Inject the Preview button.
+   */
+  public static function modifyParagraphsTop(&$element, $form_state, $context) {
+    $element['top']['#attributes']['class'][] = 'paragraphs-previewer--button-added';
+    $container = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'paragraphs-previewer-popup__link-container'
+        ],
+      ],
+      '#attached' => [
+        'library' => [
+          'paragraphs_previewer_popup/admin',
+        ],
+      ],
+      'previewer_button' => self::getPreviewButton($context, $element, $form_state),
+    ];
+
+    // Compatibility with "experimental" widget.
+    if (isset($element['top']['actions']['actions'])) {
+      $container['#weight'] = 2;
+      $container['#attributes']['class'][] = 'paragraphs-previewer-popup__link-container--experimental';
+      $element['top']['actions']['actions']['previewer_container'] = $container;
+    }
+    else {
+      $element['top']['previewer_container'] = $container;
+    }
+  }
+
+  /**
+   * Generate the button.
+   */
+  public static function getPreviewButton($context, $element, FormStateInterface $form_state) {
+    $field_name = $context['items']->getName();
+    $parents = $element['#field_parents'];
+    $delta = $element['#delta'];
+
+    $widget_state = WidgetBase::getWidgetState($parents, $field_name, $form_state);
+    if (!isset($widget_state['paragraphs'][$delta]['mode']) ||
+        !isset($widget_state['paragraphs'][$delta]['entity'])) {
+      return $element;
     }
 
-    public static function getPreviewButton($context, $element, FormStateInterface $form_state) {
-        $field_name = $context['items']->getName();
-        $parents = $element['#field_parents'];
-        $delta = $element['#delta'];
+    $paragraphs_entity = $widget_state['paragraphs'][$delta]['entity'];
+    $element_parents = array_merge($parents, [$field_name, $delta]);
+    $id_prefix = implode('-', $element_parents);
 
-        $widget_state = WidgetBase::getWidgetState($parents, $field_name, $form_state);
-        if (!isset($widget_state['paragraphs'][$delta]['mode']) ||
-                !isset($widget_state['paragraphs'][$delta]['entity'])) {
-            return $element;
-        }
+    $previewer_element = [
+      '#type' => 'submit',
+      '#value' => t('Preview'),
+      '#name' => strtr($id_prefix, '-', '_') . '_previewer_popup',
+      '#weight' => 1,
+      '#submit' => [['\Drupal\paragraphs_previewer_popup\FormHandlers', 'submitPreviewerItem']],
+      '#field_item_parents' => $element_parents,
+      '#limit_validation_errors' => [
+        array_merge($parents, [$field_name, 'add_more']),
+      ],
+      '#delta' => $delta,
+      '#ajax' => [
+        'callback' => ['\Drupal\paragraphs_previewer_popup\FormHandlers', 'ajaxSubmitPreviewerItem'],
+        'wrapper' => $widget_state['ajax_wrapper_id'],
+        'effect' => 'fade',
+      ],
+      '#access' => $paragraphs_entity->access('view'),
+      '#attributes' => [
+        'class' => ['paragraphs-previewer-popup'],
+      ],
+      '#previewer_dialog_title' => isset($element['top']['paragraph_type_title']['info']['#markup']) ? strip_tags($element['top']['paragraph_type_title']['info']['#markup']) : t('Preview'),
+    ];
 
-        $paragraphs_entity = $widget_state['paragraphs'][$delta]['entity'];
-        $element_parents = array_merge($parents, [$field_name, $delta]);
-        $id_prefix = implode('-', $element_parents);
+    return $previewer_element;
+  }
 
-        $previewer_element = [
-            '#type' => 'submit',
-            '#value' => t('Preview'),
-            '#name' => strtr($id_prefix, '-', '_') . '_previewer_popup',
-            '#weight' => 1,
-            '#submit' => [['\Drupal\paragraphs_previewer_popup\FormHandlers', 'submitPreviewerItem']],
-            '#field_item_parents' => $element_parents,
-            '#limit_validation_errors' => [
-                array_merge($parents, [$field_name, 'add_more']),
-            ],
-            '#delta' => $delta,
-            '#ajax' => [
-                'callback' => ['\Drupal\paragraphs_previewer_popup\FormHandlers', 'ajaxSubmitPreviewerItem'],
-                'wrapper' => $widget_state['ajax_wrapper_id'],
-                'effect' => 'fade',
-            ],
-            '#access' => $paragraphs_entity->access('view'),
-            '#attributes' => [
-                'class' => ['paragraphs-previewer-popup'],
-            ],
-            '#previewer_dialog_title' => isset($element['top']['paragraph_type_title']['info']['#markup'])?
-                strip_tags($element['top']['paragraph_type_title']['info']['#markup']) : t('Preview'),
-        ];
-
-        return $previewer_element;
-    }
 }
--- src/FormHandlers.php
+++ src/FormHandlers.php
@@ -7,62 +7,73 @@ use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Drupal\Core\Url;
 
+/**
+ * Functions for form callbacks.
+ */
 class FormHandlers {
-    public static function ajaxSubmitPreviewerItem(array $form, FormStateInterface $form_state) {
-        $preview_url = NULL;
-        $dialog_options = [
-            'dialogClass' => 'paragraphs-previewer-popup-ui-dialog',
-            'minWidth' => 320,
-            'width' => '98%',
-            'minHeight' => 100,
-            'autoOpen' => TRUE,
-            'modal' => TRUE,
-            'draggable' => TRUE,
-            'autoResize' => FALSE,
-            'resizable' => TRUE,
-            'closeOnEscape' => TRUE,
-            'closeText' => '',
-        ];
-        
-        $previewer_element = $form_state->getTriggeringElement();
-        $dialog_title = $previewer_element['#previewer_dialog_title'];
-        
-        // Build previewer callback url.
-        if (!empty($previewer_element['#field_item_parents']) && !empty($form['#build_id'])) {
-            $route_name = 'paragraphs_previewer_popup.form_preview';
-            $route_parameters = [
-                'form_build_id' => $form['#build_id'],
-                'element_parents' => implode(':', $previewer_element['#field_item_parents']),
-            ];
-            $preview_url = Url::fromRoute($route_name, $route_parameters);
-        }
-        
-        // Build modal content.
-        $dialog_content = [
-            '#theme' => 'paragraphs_previewer_popup_modal_content',
-            '#preview_url' => $preview_url,
-            '#attached' => [
-                'library' => ['paragraphs_previewer_popup/dialog'],
-            ],
-        ];
-        
-        // Build response.
-        $response = new AjaxResponse();
-        
-        // Attach the library necessary for using the OpenModalDialogCommand and
-        // set the attachments for this Ajax response.
-        $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
-        $response->setAttachments($form['#attached']);
-        
-        // Add modal dialog.
-        $response->addCommand(new OpenModalDialogCommand($dialog_title, $dialog_content, $dialog_options));
-        
-        return $response;
+
+  /**
+   * Ajax submit callback.
+   */
+  public static function ajaxSubmitPreviewerItem(array $form, FormStateInterface $form_state) {
+    $preview_url = NULL;
+    $dialog_options = [
+      'dialogClass' => 'paragraphs-previewer-popup-ui-dialog',
+      'minWidth' => 320,
+      'width' => '98%',
+      'minHeight' => 100,
+      'autoOpen' => TRUE,
+      'modal' => TRUE,
+      'draggable' => TRUE,
+      'autoResize' => FALSE,
+      'resizable' => TRUE,
+      'closeOnEscape' => TRUE,
+      'closeText' => '',
+    ];
+
+    $previewer_element = $form_state->getTriggeringElement();
+    $dialog_title = $previewer_element['#previewer_dialog_title'];
+
+    // Build previewer callback url.
+    if (!empty($previewer_element['#field_item_parents']) && !empty($form['#build_id'])) {
+      $route_name = 'paragraphs_previewer_popup.form_preview';
+      $route_parameters = [
+        'form_build_id' => $form['#build_id'],
+        'element_parents' => implode(':', $previewer_element['#field_item_parents']),
+      ];
+      $preview_url = Url::fromRoute($route_name, $route_parameters);
     }
-      
-    public static function submitPreviewerItem(array $form, FormStateInterface $form_state) {
-        if (!$form_state->isCached()) {
-            $form_state->setRebuild();
-        }
+
+    // Build modal content.
+    $dialog_content = [
+      '#theme' => 'paragraphs_previewer_popup_modal_content',
+      '#preview_url' => $preview_url,
+      '#attached' => [
+        'library' => ['paragraphs_previewer_popup/dialog'],
+      ],
+    ];
+
+    // Build response.
+    $response = new AjaxResponse();
+
+    // Attach the library necessary for using the OpenModalDialogCommand and
+    // set the attachments for this Ajax response.
+    $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
+    $response->setAttachments($form['#attached']);
+
+    // Add modal dialog.
+    $response->addCommand(new OpenModalDialogCommand($dialog_title, $dialog_content, $dialog_options));
+
+    return $response;
+  }
+
+  /**
+   * Rebuild the form.
+   */
+  public static function submitPreviewerItem(array $form, FormStateInterface $form_state) {
+    if (!$form_state->isCached()) {
+      $form_state->setRebuild();
     }
+  }
+
 }
